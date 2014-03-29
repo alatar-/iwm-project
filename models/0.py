0c2934d80102c6e2ef9a049d33a73b90a39ee7af
alatar- <alatar@alatar.eu>
2014-03-29 14:20:41 +0100

    init

---
+++ models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'My New App'
+settings.subtitle = 'powered by web2py'
+settings.author = 'you'
+settings.author_email = 'you@example.com'
+settings.keywords = ''
+settings.description = ''
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = 'f658e321-daf5-4123-a395-73c0cd4ea244'
+settings.email_server = 'localhost'
+settings.email_sender = 'you@example.com'
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
