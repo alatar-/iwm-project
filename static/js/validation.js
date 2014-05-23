821600b7ac05660329c12f98acc109acbc04ad06
Patryk Karolak <pat.karolak@gmail.com>
2014-05-24 01:03:18 +0200

    validation2

--- static/js/validation.js
+++ static/js/validation.js
@@ -48,32 +48,13 @@ $(document).ready(
 		$("#"+prefix+"nn_patient").after("<p>");
 		$("form").parsley({trigger: "click focus mousedown focusin focusout change keyup"});
 		checked = 0;
-		f = $("input[type=\"submit\"]");
-		$("input[type=\"submit\"]").click(function(e) {
-			if(checked == 0) {
-				$("form").parsley().validate();
-				checked = 1;
-		    	alert("Niektóre pola wydają się być wypełnione niepoprawnie. Upewnij się, że wprowadzasz poprawnie swoje dane. Jeśli na pewno chcesz zatwierdzić dane, które podałeś, ponownie zatwierdź wysłanie formularza.");
-			    $('.invalid-form-error-message')
-			    .html("You must correctly fill at least one of these 2 blocks' fields!")
-		    	.addClass("filled");
-				return false;	
-			}
-			else {
-				console.log("clicked <lol>");
-				$("form").parsley().destroy();
-				$("form").submit();
-				return true;				
-			}
-		});
-		/*
 		$("form").parsley().subscribe('parsley:form:validate', function (formInstance) {
 		    // if one of these blocks is not failing do not prevent submission
 		    // we use here group validation with option force (validate even non required fields)
 		    if (formInstance.isValid("patient", true, checked) || formInstance.isValid("nn_patient", true, checked))
 			    return true;
 		    // else stop form submission
-		    else if(!checked) {
+		    else if(checked == 0) {
 		    	checked = 1;
 		    	formInstance.submitEvent.preventDefault();
 		    	alert("Niektóre pola wydają się być wypełnione niepoprawnie. Upewnij się, że wprowadzasz poprawnie swoje dane. Jeśli na pewno chcesz zatwierdzić dane, które podałeś, ponownie zatwierdź wysłanie formularza.");
@@ -82,6 +63,11 @@ $(document).ready(
 		    	.addClass("filled");
 				return false;
 			}
-		 });*/
+			if(checked == 1) {
+				$("form").parsley().destroy();
+				$("form").submit();
+			}
+
+		 });
 	}
 );
