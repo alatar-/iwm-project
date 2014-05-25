f24a2fbe02633437697577b2b8e73f52463f8890
alatar- <alatar@alatar.eu>
2014-05-25 19:58:40 +0200

    Auto age feature

--- static/js/age.js
+++ static/js/age.js
@@ -1,35 +1,19 @@
 $(document).ready(
     function() {
-        $("#"+prefix+"nn_patient").before("<p>");
-        $("#"+prefix+"nn_patient").attr({"data-parsley-check":"[1,1]", "data-parsley-group":"nn_patient"});
-        $("#"+prefix+"nn_patient").after("<ul class=\"parsley-errors-list\"></ul>");
-        $("#"+prefix+"nn_patient").after("<p>");
-        $("form").parsley({trigger: "click focus mousedown focusin focusout change keyup"});
-        checked = 0;
-        $("form").parsley().subscribe('parsley:form:validate', function (formInstance) {
-            // if one of these blocks is not failing do not prevent submission
-            // we use here group validation with option force (validate even non required fields)
-            if (formInstance.isValid("patient", true, checked) || formInstance.isValid("nn_patient", true, checked)) {
-                $("form").parsley().destroy();
-                $("form").submit();
-                return true;
-            }
-                
-            // else stop form submission
-            else if(checked == 0) {
-                checked = 1;
-                formInstance.submitEvent.preventDefault();
-                alert("Niektóre pola wydają się być wypełnione niepoprawnie. Upewnij się, że wprowadzasz poprawnie swoje dane. Jeśli na pewno chcesz zatwierdzić dane, które podałeś, ponownie zatwierdź wysłanie formularza.");
-                $('.invalid-form-error-message')
-                .html("You must correctly fill at least one of these 2 blocks' fields!")
-                .addClass("filled");
-                return false;
-            }
-            if(checked == 1) {
-                $("form").parsley().destroy();
-                $("form").submit();
-            }
+        pesel = $("#auth_user_pesel__row td.w2p_fw").text(); 
+        var year = Number(pesel.substr(0,2));
+        if (year < 14) {
+            year += 2000;
+        } else {
+            year += 1900;
+        }
+        var month = Number(pesel.substr(2,2)) - 1;
+        var day = Number(pesel.substr(4,2));
 
-         });
+        var today = new Date();
+        var age = today.getFullYear() - year;
+        if (today.getMonth() < month || (today.getMonth() == month && today.getDate() < day)) {age--;}
+        $("#auth_user_pesel__row").after('<tr id="wiek"><td class="w2p_fl"><label for="wiek"">Wiek: </label></td><td class="w2p_fw">' + age.toString() + '</td><td class="w2p_fc"></td></tr>')
+        console.log(age);
     }
 );
